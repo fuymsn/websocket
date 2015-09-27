b89d137f9231d25f8fa37ba2cce480cd534d3c78
Young Foo <mr.f.young@gmail.com>
2015-09-27 17:26:19 +0800

    websocket first version
    websocket first version

---
+++ websocket.js
@@ -0,0 +1,77 @@
+(function (__WS, window) {
+
+    $.extend(__WS, {
+
+        init: function(){
+
+        },
+
+        onOpen: function () {
+            console.log("Connect success!");
+        },
+
+        onClose: function () {
+            console.log("Disconnected!");
+        },
+
+        onMessage: function () {
+            console.log("Retrieved data from server!");
+        },
+
+        onError: function () {
+            console.log("Sth error!");
+        }
+
+    });
+
+    /**
+     * ²ßÂÔÄ£Ê½
+     */
+
+    var Web = function (url, protocol) {
+
+        this.ws;
+
+        this.isConnection = false;
+        
+        this.init = function (url, protocol) {
+            this.ws = new WebSocket(url, protocol);
+        }
+
+        this.send = function (msg) {
+            if (this.isConnection) {
+                this.ws.send(msg);
+            }
+        }
+
+        this.ws.onopen = function () {
+            __WS.onOpen();
+            isConnection = true;
+            //todo
+        }
+
+        this.ws.onclose = function () {
+            __WS.onClose();
+            isConnection = false;
+            //todo
+        }
+
+        this.ws.onmessage = function () {
+            __WS.onMessage();
+            //todo
+        }
+
+        this.ws.onclose = function () {
+            __WS.onClose();
+            //todo
+        }
+
+        this.init();
+
+    }
+
+
+    window.WS = __WS;
+
+
+})(typeof WS !== "undefined" ? WS : {}, window);
